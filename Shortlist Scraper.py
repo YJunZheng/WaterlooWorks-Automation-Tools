22f23379fe7df070d9a1bbfac11d355ddd67b21a
Jun Zheng <56985601+YJunZheng@users.noreply.github.com>
2020-09-20 15:32:22 -0400

    updated

--- Shortlist Scraper.py
+++ Shortlist Scraper.py
@@ -1,4 +1,6 @@
 from bs4 import BeautifulSoup
+import os
+import sys
 
 def getContent(file):
 	return open(file, "r").read()
@@ -11,25 +13,15 @@ def getShortlist(content):
 		info = tr.findAll("td")
 		listing = ""
 
-		listing += info[2].text + " - "
-
-		listing += info[3].findAll("span")[-1].text.strip()
-
-		if info[8].text == "":
-			listing += "\n\t\t " + "Unknown" + " - "
-		else:
-			listing += "\n\t\t " + info[8].text + " - "
-
-		listing += info[6].text + " : " + info[10].text + "\n"
-
+		listing += info[2].text
 		shortlist.append(listing)
 
 	return shortlist
 
-html = "University of Waterloo - MyAccount - Hire Waterloo Co-op - Jobs _ Applications.html"
+html = os.path.join(sys.path[0], "shortlist.html")
 
 content = getContent(html)
 file = open("shortlist.txt", "w")
 
 for listing in getShortlist(content):
-	file.write(listing + "\n")
+	file.write(listing + " ")
